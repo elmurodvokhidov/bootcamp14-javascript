86ac91a31c8ec233a9e3402af31693f939cc3670
elmurodvokhidov <elmurodvkh@gmail.com>
2024-07-12 10:30:44 +0500

    birinchi dars

---
+++ README.md
@@ -0,0 +1,2 @@
+# Birinchi dars
+## Javascript kirish, alert, console, var, let, const, Math, Arifmetik operatorlar, Ma'lumot turlari (string, number, NaN, undefined)
---
+++ script.js
@@ -0,0 +1,40 @@
+// alert("Assalomu alaykum, O'zbekiston!"); // alert() - foydalanuvchiga xabar ko'rsatish
+// alert(5 - 20);
+// console.log("Assalomu alaykum, O'zbekiston!");
+// console.log("Hello" - "World");
+
+// let a = 5, b = 10, c = a + b;
+
+// console.log(c);
+
+// Begin1
+// let a = 10;
+// let P = 4 * a;
+// console.log(P);
+
+// Begin2
+// let a = 25;
+// let S = Math.pow(a, 2);
+// console.log(S);
+
+// let a = 6.745;
+// let S = Math.pow(a, 2); // Math.pow() - darajaga oshirish
+// console.log(S);
+// console.log(a);
+
+// let natija = (a + b) / 2;
+
+// Begin16
+// let x1 = 15, x2 = -10;
+// let javob = Math.abs(x2 - x1); // Math.abs() - vazifasi moduldan chiqarish
+// console.log(javob);
+
+// let a = 5;
+// let b = 10;
+// console.log(a);
+
+// Begin27
+// let A = 2;
+// console.log(A ** 2);
+// console.log(A ** 4);
+// console.log(A ** 8);
