19efd98dbd8c0245ff1d64d065dc148cc36fee3a
elmurodvokhidov <elmurodvkh@gmail.com>
2024-07-18 19:43:16 +0500

    to'rtinchi dars

---
+++ README.md
@@ -0,0 +1,2 @@
+# To'rtinchi dars
+## Conditional (shart) operators - if...else, ternary (?:), prompt(), confirm(), String methods (trim, includes, length, toLowerCase, toUpperCase), Math.random()
---
+++ main.js
@@ -0,0 +1,72 @@
+// if1
+// let x = -10;
+
+// if (x > 0) {
+//     console.log(++x);
+// }
+
+// if2
+// let x = 5;
+
+// if (x > 0) {
+//     console.log(++x);
+// }
+// else {
+//     console.log(x - 2);
+// }
+
+// if4
+// let a = 1, b = -2, c = 3;
+// let d = 0;
+
+// if (a > 0) {
+//     ++d
+// }
+// if (b > 0) {
+//     ++d
+// }
+// if (c > 0) {
+//     ++d
+// }
+
+// console.log(d);
+
+// if24
+// let x = -6;
+
+// if (x > 0) {
+//     console.log(2 * x);
+// }
+// else if (x <= 0) {
+//     console.log(x - 6);
+// }
+
+// Ternery operators
+// let isAdmin = true;
+// let natija = isAdmin === true ? "Salom admin" : "Salom askar";
+// console.log(natija);
+
+// prompt() - foydalanuvchidan qiymat olish (string);
+// let userName = prompt("Ismingizni kiriting:");
+// let email = prompt("Elektron pochtangizni kiriting:");
+// let password = prompt("Parolingizni kiriting:");
+// console.log(userName);
+
+// confirm() - foydalanuvchi tasdiqlashini so'rash
+// let isDeleted = confirm("Rostdan ham o'chirishgan ishonching komilmi?");
+// if (isDeleted) {
+//     console.log("Ma'lumotlar o'chirib tashlandi");
+// }
+// else {
+//     console.log("Yangi yilingiz bilan");
+// }
+
+// String methods
+// let ism = "elmurod";
+// console.log(ism.trim());
+// console.log(ism.includes("b"));
+// console.log(ism.length);
+// console.log(ism.toLowerCase());
+// console.log(ism.toUpperCase());
+
+// console.log(Math.round(Math.random() * 10));
