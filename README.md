36c247cbb7f62919433a650c965be32050a509b0
elmurodvokhidov <elmurodvkh@gmail.com>
2024-07-31 09:58:44 +0500

    to'qqizinchi dars

---
+++ README.md
@@ -0,0 +1,2 @@
+# To'qqizinchi dars
+## Array
---
+++ main.js
@@ -0,0 +1,52 @@
+// let ismlar = ["elmurod", "ilhombek"];
+// const sonlar = new Array(1, 2, 3, 4, 5);
+// console.log(sonlar);
+
+// Mixed array
+// var mixedArr = ["text", 21, undefined, true, null];
+// console.log(mixedArr[2]);
+
+// const sonlar = new Array(1, 2, 3, 4, 5);
+// console.log(sonlar[3] + sonlar[1]);
+// console.log(ismlar[5]);
+
+// let ismlar = ["elmurod", "ilhombek"];
+// console.log(typeof ismlar[0]);
+
+// Length
+// let mashinaNomlari = ["Damas", "Tico", "Nexia", "Matiz", "Lacetti"];
+// console.log(mashinaNomlari[mashinaNomlari.length - 1]);
+
+// push
+// let futbolchilar = ["Ronaldo", "Messi", "Neymar", "Pele"];
+// futbolchilar.push("M.Salah");
+
+// unshift
+// futbolchilar.unshift("Ronaldinho");
+
+// pop
+// let removedItem = futbolchilar.pop();
+
+// shift
+// let removedItem = futbolchilar.shift();
+
+// futbolchilar[1] = "Foden";
+// futbolchilar[6] = "Rashford";
+// console.log(futbolchilar);
+
+// Nested Array
+// let englandFootballClubs = [["Walker", "Debrune", "Haaland"], ["Alisson", "Virjil", "Salah"], ["Rashford", "Odilbek", "Ilhombek"]];
+// englandFootballClubs[0][3] = "Ederson";
+
+// englandFootballClubs.push(["a", "b", "c"]);
+// console.log(englandFootballClubs[3].pop());
+
+// Splice
+// let futbolchilar = ["Ronaldo", "Messi", "Neymar", "Pele"];
+// futbolchilar.splice(1, 2);
+// console.log(futbolchilar);
+
+// Slice
+// let futbolchilar = ["Ronaldo", "Messi", "Neymar", "Pele"];
+// const example = futbolchilar.slice(3, 4);
+// console.log(example);
