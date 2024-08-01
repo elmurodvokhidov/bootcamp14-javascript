90671198a832af9dbacaca8376417d900967db24
elmurodvokhidov <elmurodvkh@gmail.com>
2024-08-01 20:53:08 +0500

    o'ninchi dars

---
+++ README.md
@@ -0,0 +1,2 @@
+# O'ninchi dars
+## Array metodlari, concat, join, indexOf, includes, find, findIndex, forEach, filter, map, reverse, sort
---
+++ main.js
@@ -0,0 +1,69 @@
+// let group1 = ["Ilhombek", "Odilbek", "Muhammadali", "Ruhshona"];
+// console.log(students[0]);
+// console.log(students[1]);
+// console.log(students[2]);
+// console.log(students[3]);
+// console.log(students[4]);
+// console.log(students[5]);
+
+// for (let i = 0; i <= students.length - 1; i++) {
+//     console.log(students[i]);
+// }
+
+// let group2 = ["Osiyo", "Abdurahim", "Sardor", "Olim"];
+
+// concatination
+// let newArr = group1.concat(group2);
+
+// spread
+// group1 = [...group1, ...group2];
+
+// indexOf
+// console.log(group1.indexOf("Osiyo"));
+
+// includes
+// console.log(group1.includes("Ruhshona"));
+
+// join
+// console.log(group1.join(" va "));
+
+// reverse
+// console.log(group1.reverse());
+
+
+// forEach
+// let group1 = ["Ilhombek", "Odilbek", "Muhammadali", "Ruhshona"];
+// group1.forEach(qiymatChiqar);
+
+// function qiymatChiqar(element, index) {
+//     console.log(element);
+// }
+
+// group1.forEach(element => console.log(element));
+
+// find & findIndex
+// let group = [60, 10, 85, 11];
+
+// // let result = group.find((element) => element < 10);
+// let result = group.findIndex((element) => element < 10);
+
+// console.log(result);
+
+// map
+// let group = ["Ilhombek", "Odilbek", "Muhammadali", "Ruhshona"];
+// let result = group.map(element => element);
+// console.log(result);
+
+// filter
+// let group = [60, 10, 85, 11, 420, 16, 21, 48];
+// let result = group.filter(element => element > 50);
+// console.log(result);
+
+// reduce
+// let group = [60, 10, 30];
+// let result = group.reduce((a, b) => a + b);
+// console.log(result);
+
+// sort
+// let arr = [40, 100, 1, 5, 11, 2, 22];
+// console.log(arr.sort((a, b) => a - b));
