628c2c6304d2e23a16a63352cb9b23e9c9a9ed6e
elmurodvokhidov <elmurodvkh@gmail.com>
2024-08-11 09:53:08 +0500

    o'n to'rtinchi dars

---
+++ README.md
@@ -0,0 +1,2 @@
+# O'n to'rtinchi dars
+## cssText, createElement(), appendChild(), removeChild(), getAttribute(), setAttribute(), classList, add(), remove(), toggle()
---
+++ app.js
@@ -0,0 +1,82 @@
+// const headingOne = document.getElementById('headingOne');
+
+// ? Javascript yordamida css ishlatishning birinchi usuli
+// headingOne.style.color = "green";
+// headingOne.style.fontFamily = "sans-serif";
+// headingOne.style.textAlign = "center";
+// headingOne.style.fontSize = "50px";
+// headingOne.style.border = "2px dotted green";
+// headingOne.style.margin = "20px";
+// headingOne.style.padding = "20px";
+
+// ? Javascript yordamida css ishlatishning ikkinchi usuli
+// headingOne.style.cssText = `
+//     color: green;
+//     font-family: sans-serif;
+//     text-align: center;
+//     font-size: 50px;
+//     border: 2px dotted green;
+//     margin: 20px;
+//     padding: 20px;
+// `
+
+
+
+
+
+
+
+
+
+
+// const unorderedList = document.getElementsByTagName('ul')[0];
+// ? Yangi element yaratish
+// const newListItem = document.createElement("li");
+// newListItem.textContent = "Javascript";
+
+// ? Hosil bo'lgan yangi elementni ro'yhatga qo'shish
+// unorderedList.appendChild(newListItem);
+
+// ? Ro'yhatdan mavjud elementni o'chirish
+// unorderedList.removeChild(document.getElementsByTagName("li")[1]);
+
+
+
+
+
+
+
+
+
+
+// ? Element attributlarini olish
+// const input = document.querySelector("#username");
+// console.log(input.getAttribute("class"));
+
+// ? Elementga yangi attribut tayinlash
+// input.setAttribute("class", "yangiClass");
+
+
+
+
+
+
+
+
+
+
+// ! classList xossasi elementlarning class'lar ro'yhatini qaytaradi, uning quyidagi 3ta metodi mavjud:
+// const classess = input.classList;
+// console.log(classess);
+
+// ? add
+// input.classList.add("newClass");
+
+// ? remove
+// input.classList.remove("yangiClass");
+
+// ? toggle
+// input.classList.toggle("box");
+// input.classList.toggle("box");
+// input.classList.toggle("box");
+// input.classList.toggle("box");
