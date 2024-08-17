81bf5fd604ba0b36c6c7a29cdaf55d41a087d132
elmurodvokhidov <elmurodvkh@gmail.com>
2024-08-17 22:09:01 +0500

    o'n beshinchi dars

---
+++ README.md
@@ -0,0 +1,2 @@
+# O'n beshinchi dars
+## Events, ranglar generatori amaliy loyiha
---
+++ main.js
@@ -0,0 +1,29 @@
+// const text = document.querySelector("#text");
+// function translateFunction() {
+//     text.textContent = "Salom";
+// }
+
+// function myFunction() {
+//     text.textContent = "Hello";
+// }
+
+// const box = document.querySelector(".box");
+// box.onmousewheel = changeColorFunction;
+
+// function changeColorFunction() {
+//     let colorsItem = [1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+//     let randomColor = "#";
+//     for (let i = 0; i < 6; i++) {
+//         let randomNumbers = Math.round(Math.random() * (colorsItem.length - 1));
+//         randomColor += colorsItem[randomNumbers];
+//     }
+//     box.style.background = randomColor;
+//     box.textContent = randomColor;
+// }
+
+// const body = document.querySelector('body');
+// body.onkeydown = myFunction;
+
+// function myFunction(e) {
+//     console.log(e);
+// }
